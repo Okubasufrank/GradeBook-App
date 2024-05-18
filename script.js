d579657bbaff4536f828800a066e09b96ece7e6b
Frankie <franklineokubasu51@gmail.com>
2024-05-18 16:04:58 +0300

    looping through the array and sum all the scores

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function getAverage(scores){
+    let total = 0;
+    
+    //loop through the array and sum all the scores
+    for (let i = 0; i < scores.length; i++){
+        total += scores[i];
+    }
 
+    // calculate avareage
 }
 
+console.log(([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
+console.log(([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]))
